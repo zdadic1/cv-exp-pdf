f14e2a6977958ff36f13d35566469939a2f49b09
zdadic1 <shvone@gmail.com>
2020-04-14 23:43:39 +0200

    CSS fixes
    a lot of shit

--- src/Form.js
+++ src/Form.js
@@ -12,8 +12,9 @@ export default function Form() {
           className="occupation-input"
           placeholder="Enter occupation"
         ></input>
+
         <div className="profile">P R O F I L E</div>
-        <ColoredLine />
+        <hr />
 
         <input
           type="text"
@@ -25,27 +26,27 @@ export default function Form() {
           className="address-input"
           placeholder="&#x2302; Enter address"
         ></input>
-        <ColoredLine />
+        <hr />
         <input
           className="phone-input"
           placeholder="&#x260F; Enter phone"
         ></input>
-        <ColoredLine />
+        <hr />
         <input
           className="email-input"
           placeholder="&#x2709; Enter email"
         ></input>
-        <ColoredLine />
+        <hr />
 
         <div className="profile">S K I L L S</div>
-        <ColoredLine />
+        <hr />
         <input
           className="summary-input"
           placeholder="Put your skillset here..."
         ></input>
 
         <div className="profile">E D U C A T I O N</div>
-        <ColoredLine />
+        <hr />
         <input
           className="summary-input"
           placeholder="Education info..."
@@ -53,29 +54,28 @@ export default function Form() {
       </div>
       <div className="column-right">
         <div className="right-side"> E X P E R I E N C E</div>
-        <ColoredLine />
+        <hr />
         <input
           className="summary-input"
           placeholder="Write about your most notable projects..."
         ></input>
         <input className="summary-input"></input>
+
         <div className="right-side">L A N G U A G E S</div>
-        <ColoredLine />
+        <hr />
         <input
           className="summary-input"
           placeholder="What languages do you speak and how proficient are you?"
         ></input>
+
         <div className="right-side">R E F E R E N C E S</div>
-        <ColoredLine />
+        <hr />
         <input
           className="summary-input"
           placeholder="State your references here..."
         ></input>
       </div>
-      {/*       </div>
-       */}
       <div className="button-export"></div>
     </div>
   );
 }
-const ColoredLine = ({ color: black }) => <hr />;
